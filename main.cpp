f1aeea11abd65a0da5dd664d98338f634152cffc
MingJuSun <mingju.s@outlook.com>
2026-08-24 01:07:04 +0800

    SDL的基本流程：初始化、创建窗口和渲染器、游戏循环以及清理和退出

--- main.cpp
+++ main.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
+#include <SDL.h>
 
-int main()
+int main(int, char **)
 {
     std::cout << "Hello, World!" << std::endl;
+
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Create a window
+    SDL_Window *window = SDL_CreateWindow("Hello World!", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+
+    // Create a renderer
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // Render loop
+    while (true)
+    {
+        SDL_Event event;
+        if (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                break;
+            }
+        }
+
+        // Clear the renderer
+        SDL_RenderClear(renderer);
+        // Draw a RECTANGLE
+        SDL_Rect rect = {100, 100, 200, 200};
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+        // Update the renderer
+        SDL_RenderPresent(renderer);
+    }
+
+    // Destroy the renderer
+    SDL_DestroyRenderer(renderer);
+
+    // Destroy the window
+    SDL_DestroyWindow(window);
+
+    // Quit SDL
+    SDL_Quit();
+
     return 0;
 }
